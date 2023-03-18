1d08e3477c76fd91bc1b10bfddd48377685fb23a
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-18 17:15:06 +0200

    Update pw.sh

--- pw.sh
+++ pw.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 adduser "$1"
-echo "$2" | passwd "$1" --stdin  2> 30.txt \n iconv -f UTF-8 -t CP1251 30.txt > 40.txt
+echo "$2" | passwd "$1" --stdin  2> 30.txt
+iconv -f UTF-8 -t CP1251 30.txt > 40.txt
